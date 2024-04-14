cf8d572b59ac50522b201a5e9f733590bccc8e57
Vitória Camelo <93448845+VitoriaCamelo@users.noreply.github.com>
2024-04-14 16:09:01 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,52 +1,9 @@
 import Text.Printf
 
--- recebe um comando e devolve a posição final
-basico :: Command -> (Int, Int, Direcao, [Command]) 
-basico (Forward n) = (0, n, Norte, [])               -- se comando foi Forward, incrementa y
-basico (Backward n) = (0, -n, Norte, [])             -- se comando foi Backward, decrementa y
-basico TurnLeft = (0, 0, Oeste, [])       
-basico TurnRight = (0, 0, Leste, [])
-
--- posicionar recebe posição atual, direção e lista de comandos 
--- e devolve posição final, direção e lista de comandos
-posicionar :: (Int, Int, Direcao, [Command]) -> (Int, Int, Direcao, [Command])
--- caso base: sem comandos, permanecer na posição recebida
-posicionar (x, y, direcao, []) = (x, y, direcao, []) 
--- se estiver olhando para o norte e for para a frente, incrementa y
-posicionar (x, y, Norte, (Forward n):xs) = posicionar (x, y+n, Norte, xs)
--- se estiver olhando para o norte e for para trás, decrementa y
-posicionar (x, y, Norte, (Backward n):xs) = posicionar (x, y-n, Norte, xs)
--- se estiver olhando para o sul e for para a frente, decrementa y
-posicionar (x, y, Sul, (Forward n):xs) = posicionar (x, y-n, Sul, xs)
--- se estiver olhando para o sul e for para trás, incrementa y
-posicionar (x, y, Sul, (Backward n):xs) = posicionar (x, y+n, Sul, xs)
--- se estiver olhando para o leste e for para a frente, incrementa x
-posicionar (x, y, Leste, (Forward n):xs) = posicionar (x+n, y, Leste, xs)
--- se estiver olhando para o leste e for para trás, decrementa x
-posicionar (x, y, Leste, (Backward n):xs) = posicionar (x-n, y, Leste, xs)
--- se estiver olhando para o oeste e for para a frente, decrementa x
-posicionar (x, y, Oeste, (Forward n):xs) = posicionar (x-n, y, Oeste, xs)
--- se estiver olhando para o oeste e for para trás, incrementa x
-posicionar (x, y, Oeste, (Backward n):xs) = posicionar (x+n, y, Oeste, xs)
-
--- Ex.: Norte + TurnLeft = Oeste
-posicionar (x, y, Norte, (TurnLeft):xs) = posicionar (x, y, Oeste, xs)
-posicionar (x, y, Norte, (TurnRight):xs) = posicionar (x, y, Leste, xs)
-posicionar (x, y, Sul, (TurnLeft):xs) = posicionar (x, y, Leste, xs)
-posicionar (x, y, Sul, (TurnRight):xs) = posicionar (x, y, Oeste, xs)
-posicionar (x, y, Leste, (TurnLeft):xs) = posicionar (x, y, Norte, xs)
-posicionar (x, y, Leste, (TurnRight):xs) = posicionar (x, y, Sul, xs)
-posicionar (x, y, Oeste, (TurnLeft):xs) = posicionar (x, y, Sul, xs)
-posicionar (x, y, Oeste, (TurnRight):xs) = posicionar (x, y, Norte, xs)
-
-data Direcao = Norte | Sul | Leste | Oeste       -- para onde o robô pode estar olhando
+data Direcao = Norte | Sul | Leste | Oeste -- para onde o robô pode estar olhando
   deriving (Show)
-data Command = Forward Int | Backward Int | TurnLeft | TurnRight  -- comandos possíveis
-  deriving (Show)
-destination :: [Command] -> (Int, Int, Direcao, [Command]) 
-destination [] = (0, 0, Norte, [])    -- caso base: sem comandos, permanecer na posição original
-destination (x:[]) = basico x         -- caso básico: apenas um comando foi recebido
-destination (x:xs) = posicionar (0, 0, Norte, (x:xs)) -- caso composto: chama função posicionar
+data Command = Forward Int | Backward Int | TurnLeft | TurnRight  | Stop -- comandos possíveis
+  deriving (Eq, Show)
 
 arvore :: String
 arvore = "#"
@@ -80,10 +37,23 @@ cenario (x:y:z:t:_) = do
   printf " ____________\n"
 
 trataSimples :: Command -> [[String]] -> [[String]]
-trataSimples TurnLeft montagem = map (map (\x -> if x == alunoNorte then alunoLeste else if x == alunoSul then alunoLeste else if x == alunoLeste then alunoNorte else alunoSul)) montagem
+trataSimples TurnLeft montagem = 
+   map (map (\x -> 
+    if x == alunoNorte then alunoOeste 
+    else if x == alunoSul then alunoLeste 
+    else if x == alunoLeste then alunoNorte 
+    else if x == alunoOeste then alunoSul 
+    else x)) montagem
+trataSimples TurnRight montagem = 
+   map (map (\x -> 
+     if x == alunoNorte then alunoLeste 
+     else if x == alunoSul then alunoOeste 
+     else if x == alunoLeste then alunoSul 
+     else if x == alunoOeste then alunoNorte 
+     else x )) montagem
 
 trataComando :: Command -> [[String]] -> [[String]]
-trataComando TurnLeft montagem = trataSimples TurnLeft montagem1
+trataComando TurnLeft montagem = trataSimples TurnLeft montagem
 trataComando TurnRight montagem = trataSimples TurnRight montagem
 
 
@@ -92,6 +62,7 @@ menu = do
   putStrLn "\nOpções:"
   putStrLn "\n[1] Para Frente \t\t[2] Para Trás"
   putStrLn "\n[3] Virar à Esquerda \t[4] Virar à Direita"
+  putStrLn "\n[5] Parar"
   putStrLn "\nEscolha um comando:"
   comandoStr <- getLine                
   let comando = read comandoStr :: Int
@@ -103,24 +74,23 @@ menu = do
       return (Forward passos)
     else 
       return (Backward passos)
-  else 
-    if comando == 3 then 
-      return TurnLeft
-    else 
-      return TurnRight
+  else if comando == 3 then 
+    return TurnLeft
+  else if comando == 4 then
+    return TurnRight
+  else
+    return Stop
 
 jogo :: [[String]] -> IO()
 jogo montagem = do
   cenario montagem
   comando <- menu
-  trataComando comando montagem
-  cenario 
+  if comando == Stop then do
+    putStrLn "\nAté a próxima!"
+  else do
+    jogo (trataComando comando montagem)
   
 main = do
   putStrLn "-- Seja bem-vinda(o) ao jogo Chegando ao CI! --"
   putStrLn "\nFase 1:"
   jogo montagem1
-  
-  
-  --print(destination [TurnLeft, Backward 3, TurnRight, TurnRight, Forward 4])
-  --putStrLn "\128187"
